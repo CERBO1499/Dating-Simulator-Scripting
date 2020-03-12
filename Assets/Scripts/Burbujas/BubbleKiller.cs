a194d73a667257968914b90592fc83ff5915f565
CERBO1499 <berriocarlos1499@gmail.com>
2020-03-11 23:09:11 -0500

    cerbo

--- Assets/Scripts/Burbujas/BubbleKiller.cs
+++ Assets/Scripts/Burbujas/BubbleKiller.cs
@@ -7,24 +7,27 @@ using UnityEngine;
 
 public class BubbleKiller : MonoBehaviour
 {
-    
 
+    private int countLose;
+    [SerializeField] private int Umbral;
     private void OnTriggerEnter(Collider other)
     {
         
         if (other.tag == "Bubble")
         {
             Bubble bl = other.gameObject.GetComponent<Bubble>();
-            switch (bl.Trait)
+
+
+            if (bl.Trait == Etrait.Love || bl.Trait == Etrait.Intelligence || bl.Trait == Etrait.Filrt)
             {
-                case Etrait.Love:
-                case Etrait.Intelligence:
-                case Etrait.Filrt:
-                    break;
-                default:
-                    break;
-                    
+                countLose++;
             }
+
+            if (countLose >= Umbral)
+            {
+                //Quite vida
+            }
+            
             bl.Die(); 
             
             
